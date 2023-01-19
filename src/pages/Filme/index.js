dd94d1bf7298b5e8623ec1f958f398096091dae3
Lucas Bicalho <bicalho.lucas@gmail.com>
2023-01-19 18:37:59 -0300

    Detalhes do filme

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,65 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import './filme-info.css';
+import api from '../../services/api'
+
 function Filme () {
+    const { id } = useParams();
+    const[filme, setFilme] = useState({});
+    const[loading, setLoading] = useState(true);
+
+    useEffect(()=> {
+        async function loadFilme() {
+            await api.get(`/movie/${id}`, {
+                params: {
+                    api_key: "f4bff4f5e72cd5895a1208b6c2065cbf",
+                    language: "pt-BT",
+            }
+         })
+         .then((response)=> {
+            setFilme(response.data);
+            setLoading(false);
+         })
+         .catch(()=>{
+            console.log("FILME NÃO ENCONTRADO")
+         })
+    }
+
+        loadFilme();
+
+
+        return () => {
+            console.log("COMPONENTE FOI DESMONTADO")
+        }
+    }, [])
+
+    if(loading) {
+        return(
+            <div className="filme-info">
+                <h1>Carregando detalhes...</h1>
+            </div>
+        )
+    }
+
     return(
-        <div>
-            <h1>BEM VINDO A PAGINA DE FILMES</h1>
+        <div className="filme-info">
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title}/>
+
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+
+            <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+            <div className="area-buttons">
+                <button>Salvar</button>
+                <button>
+                    <a href="#">
+                        Trailer
+                    </a>
+                </button>
+            </div>
+
         </div>
     )
 }
